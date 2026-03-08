8f0b5c3cd5f654e3311a4332bbcf011f41a01f4c
Anusha <anushadharemane@gmail.com>
2026-03-08 11:51:07 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+# function getsum to get sum of numbers
+
+def getsum(a,b):
+    return a+b
+
+sum1=getsum(2,3)
+print("sum of two integer numbers:",sum1)
+
+sum2=getsum(1.5,3.2)
+print("sum of two float numbers :",sum2)
+
+sum3=getsum('a','b')
+print("sum of two numbers:",sum3)
