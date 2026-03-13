9242973fc4ac1bcdee9f3143d52389d1652d8353
Anusha <anushadharemane@gmail.com>
2026-03-13 14:09:19 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,7 @@
+#function simpleGreetings to accept name as input and print simple greetings Namaskara name
+
+def simpleGreetings():
+    name=input("Enter a name:")
+    print(f"Namaskara,{name}!")
+
+simpleGreetings()
